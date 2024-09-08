f3f1fdab1a888709bf6dff96b22a32f1d6c887b1
yoelmarain <jmarain62@gmail.com>
2024-09-08 10:24:12 -0300

    refactor: Remove unused NavBar links

--- vite-project/src/components/NavBar/LinksItems.tsx
+++
@@ -1,9 +0,0 @@
-import persona from '../icons/2.png';
-import { HiOutlineUser } from "react-icons/hi";
-
-export const LINK_ITEMS = [
-  { icon: '', url: 'escultores', title: 'Escultores' },
-  { icon: '', url: 'obras', title: 'Obras' },
-  { icon: '', url: 'eventos', title: 'Eventos' },
-  { icon: '', url: 'usuarios', title: 'Usuarios' },
-];
