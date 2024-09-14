0e591841569632e8d418d845719f06f89521d01c
yoelmarain <jmarain62@gmail.com>
2024-09-14 19:02:55 -0300

    feat: Add react-dropzone package for file uploads and implement file preview and removal functionality

--- FrontEnd/src/components/ZonaCarga/ZonaCarga.tsx
+++ FrontEnd/src/components/ZonaCarga/ZonaCarga.tsx
@@ -1,17 +1,30 @@
 import { useDropzone , FileRejection} from 'react-dropzone';
-import { Flex, Box, VStack, useToast, Stack, SimpleGrid } from '@chakra-ui/react';
+import { Flex, Box, VStack, useToast, Stack, SimpleGrid, IconButton } from '@chakra-ui/react';
 import { useState, useCallback } from 'react';
 import { HiOutlineDocumentAdd } from "react-icons/hi";
+import { CloseIcon } from '@chakra-ui/icons';
 
 // Los archivos seleccionados se guardan en el estado/array files, y las previsualizaciones en filePreviews
+// Se le debe pasar por props el maximo de archivos que se pueden cargar, por defecto es 10
 
-const ZonaCarga = () => {
+const ZonaCarga = ({maxFiles = 10}) => {
     const toast = useToast();
     const [filePreviews, setFilePreviews] = useState<string[]>([]);  // Guarda las URL de las previsualizaciones 
     const [files, setFiles] = useState<File[]>([]);  // Guarda los archivos seleccionados para dsp mandar al back
 
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        if (acceptedFiles.length > 0) {
+
+            if (files.length + acceptedFiles.length > maxFiles) {
+                toast({
+                    title: 'Error',
+                    description: `Solo se pueden cargar hasta ${maxFiles} ${maxFiles === 1 ? 'archivo' : 'archivos'}`,
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                });
+                return;
+            }
+            
             const newFiles = acceptedFiles.map((file) => {
                 const previewUrl = URL.createObjectURL(file);
                 return { file, previewUrl };
@@ -20,10 +33,10 @@ const ZonaCarga = () => {
             setFilePreviews((prevPreviews) => [
                 ...prevPreviews,
                 ...newFiles.map((fileObj) => fileObj.previewUrl),
-            ]);
+           ]);
             setFiles((prevFiles) => [...prevFiles, ...newFiles.map((fileObj) => fileObj.file)]);
-        }
-    }, []);
+        
+    }, [files, maxFiles, toast]);
 
     
     const onDropRejected = useCallback((rejectedFiles: FileRejection[]) => {
@@ -40,6 +53,12 @@ const ZonaCarga = () => {
 
     console.log(files);
 
+    const handleRemoveImage = (index: number) => {
+        //Elimina la previsualizacion y el archivo seleccionado
+        setFilePreviews((prevPreviews) => prevPreviews.filter((_, i) => i !== index));
+        setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+    };
+
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop,
@@ -49,6 +68,7 @@ const ZonaCarga = () => {
             'image/png': ['.png'],
             'image/webp': ['.webp'],
         },
+        maxFiles,
     });
     
     return (
@@ -78,18 +98,28 @@ const ZonaCarga = () => {
              )}
         </VStack>
         {filePreviews.length > 0 && (
-            <SimpleGrid columns={{ base: 2, md: 4 }} spacing={4} mt="20px">
-                {filePreviews.map((preview, index) => (
-                    <Box key={index}>
-                        <img
-                            src={preview}
-                            alt={`Preview ${index}`}
-                            style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-                        />
-                    </Box>
-                ))}
-            </SimpleGrid>
-        )}
+                <SimpleGrid columns={{ base: 2, md: 4 }} spacing={4} mt="20px">
+                    {filePreviews.map((preview, index) => (
+                        <Box key={index} position="relative">
+                            <IconButton
+                                icon={<CloseIcon />}
+                                size="sm"
+                                position="absolute"
+                                top="2px"
+                                right="2px"
+                                variant={'delete'}
+                                onClick={() => handleRemoveImage(index)}
+                                aria-label="Remove image"
+                            />
+                            <img
+                                src={preview}
+                                alt={`Preview ${index}`}
+                                style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                            />
+                        </Box>
+                    ))}
+                </SimpleGrid>
+            )}
         </Flex>
         </Stack>
     );
