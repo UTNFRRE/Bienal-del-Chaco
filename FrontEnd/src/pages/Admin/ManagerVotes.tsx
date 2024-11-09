223d634cef8ac61640863ec2d99e5208b0bc713d
Camilo Aguirre <33167332+Agcamilo@users.noreply.github.com>
2024-11-09 16:56:18 -0300

    CreateButtonComponents and a directoy for the charts

--- FrontEnd/src/pages/Admin/ManagerVotes.tsx
+++ FrontEnd/src/pages/Admin/ManagerVotes.tsx
@@ -1,18 +1,9 @@
-import { Text, Box, Flex, Button,useDisclosure } from '@chakra-ui/react';
-import { useState } from 'react';
-import { VscAdd } from "react-icons/vsc";
-import { BsQrCode } from "react-icons/bs";
-import QR from "../../components/Modal/QRGenerator";
+import { Text, Box, Flex } from '@chakra-ui/react';
+import CreateVoteButton from "../../components/Vote/CreateVoteButton"; //Boton para crear votacion
+import QrButton from "../../components/Vote/QrButton" //Boton para generar QR
 
 const ManagerVotes = () => {
-    const [hover, setHover] = useState(false);
-    const [hover1, setHover1] = useState(false);
-
-    const { 
-        isOpen: isOpenQr, 
-        onOpen: onOpenQr, 
-        onClose: onCloseQr 
-    } = useDisclosure();
+    
 
     return (
         <>
@@ -28,75 +19,38 @@ const ManagerVotes = () => {
                     align="center"
                     gap={4}
                 >
-                    <Button
-                        bg="#1E2A5E"
-                        variant="solid"
-                        borderRadius={3}
-                        p={4}
-                        onMouseEnter={() => setHover(true)}
-                        onMouseLeave={() => setHover(false)}
-                        transition="all 0.3s ease"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        _hover={{ bg: "#2b345b" }}
-                    >
-                        <Flex
-                            transition="opacity 0.4s ease"
-                            opacity={hover ? 0 : 1}
-                            position="absolute"
-                        >
-                            <VscAdd />
-                        </Flex>
-                        <Flex
-                            transition="opacity 0.4s ease"
-                            opacity={hover ? 1 : 0}
-                        >
-                            <Text>Crear Votacion</Text>
-                        </Flex>
-                    </Button>
-                    <Button
-                        bg="#1E2A5E"
-                        variant="solid"
-                        borderRadius={3}
-                        p={4}
-                        onMouseEnter={() => setHover1(true)}
-                        onMouseLeave={() => setHover1(false)}
-                        transition="all 0.3s ease"
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        _hover={{ bg: "#2b345b" }}
-                        onClick={onOpenQr}
-                    >
-                        <Flex
-                            transition="opacity 0.4s ease"
-                            opacity={hover1 ? 0 : 1}
-                            position="absolute"
-                        >
-                            <BsQrCode />
-                        </Flex>
-                        <Flex
-                            transition="opacity 0.4s ease"
-                            opacity={hover1 ? 1 : 0}
-                        >
-                            <Text>Generar QR</Text>
-                        </Flex>
-                    </Button>
+                    <CreateVoteButton />{/* Boton para crear votacion */} 
+                   <QrButton /> {/* Boton para generar QR */}
                 </Flex>
-                <Box
-                    bg="secundaryBg"
-                    p={6}
-                    w="80%"
-                    borderWidth={1}
-                    borderColor={"secundaryHover"}
-                >
-                    <Text>Tabla de Votaciones</Text>
-                </Box>
+                <Flex
+                    justifyContent='center'
+                    width='100vw'
+                    flexDirection={'row'}
+                    height={'75vh'}
+                    >
+                    <Box p={6} width={'15vw'}>
+                        <h1>EDICIONES:</h1>
+                        <ul>
+                            {["2021", "2022", "2023"].map((year) => (
+                                <>
+                                    
+                                    <li key={year}><a href="/"> {year}</a></li>
+                                </>
+                            ))}
+                        </ul>
+                    </Box>
+                    <Box
+                        bg="secundaryBg"
+                        p={6}
+                        w="80vw"
+                        borderWidth={1}
+                        borderColor={"secundaryHover"}
+                    >
+                        <Text>Tabla de Votaciones</Text>
+                    </Box>
+                </Flex>
             </Flex>
-            <QR 
-            isOpen={isOpenQr} 
-            onClose={onCloseQr} />
+            
         </>
     );
 }
