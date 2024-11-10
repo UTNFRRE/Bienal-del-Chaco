3595b6a039efe7edf18fc7b3cd211be6d8ad7e27
Camilo Aguirre <camiloo12.c1@gmail.com>
2024-11-10 16:49:12 -0300

    Boton por menu

--- FrontEnd/src/components/Modal/QRGenerator.tsx
+++ FrontEnd/src/components/Modal/QRGenerator.tsx
@@ -6,14 +6,12 @@ import {
     ModalFooter, 
     ModalBody, 
     ModalCloseButton, 
-    Input, 
     Button,
-    Textarea,
-    Stack,
     Box, 
-    FormLabel, 
-    Image
+    Image,
+    Select
 } from '@chakra-ui/react';
+import obras from '../../API/ObrasVote'
 import {useState} from 'react';
 
 interface QRProps {
@@ -24,8 +22,19 @@ interface QRProps {
 
 }
 
-const QR = ({isOpen,onClose}:QRProps) => {
+    const QR = ({isOpen,onClose}:QRProps) => {
 
+        const [obra,setObra] = useState<number>(0);
+        const [showQR, setShowQR] = useState(false);
+
+        const handleGenerateQR = (obra: number) => {
+            setShowQR(true);
+            console.log(obra);
+        }
+
+        const handleCancel = () => {
+            setShowQR(false);
+        }
     return (
         <>
             <Modal isOpen={isOpen} onClose={onClose}>
@@ -34,11 +43,29 @@ const QR = ({isOpen,onClose}:QRProps) => {
                     <ModalHeader>QR</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Box display="flex" justifyContent="center">
-                            <Image src="https://upload.wikimedia.org/wikipedia/commons/d/d7/Commons_QR_code.png" fit="cover"/>
+                        <Box width="100%" >
+                            <Select
+                                placeholder="Selecciona una obra"
+                                onChange={(e) =>setObra(Number(e.target.value))}
+                            >
+                                {obras.map((o) => (
+                                    <option key={o.id} value={o.id}>
+                                        {o.nombreObra}
+                                    </option>
+                                ))}
+                            </Select>
+                            <Button onClick={() => handleGenerateQR(obra)}>
+                                Generar QR
+                            </Button>
                         </Box>
+                        {showQR && (
+                            <Box display="flex" justifyContent="center">
+                                <Image src="https://upload.wikimedia.org/wikipedia/commons/d/d7/Commons_QR_code.png" fit="cover"/>
+                            </Box>
+                        )}
                     </ModalBody>
                     <ModalFooter>
+                        <Button onClick={handleCancel} colorScheme='blue'>Aceptar</Button>
                         <Button onClick={onClose} colorScheme="red">Cancelar</Button>
                     </ModalFooter>
                 </ModalContent>
