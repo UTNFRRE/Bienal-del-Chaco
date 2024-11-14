62174fd33163ab4b92566960deef04d8476254c8
agustinferrazzano <agustin.ferrazzano1213@gmail.com>
2024-11-14 09:39:28 -0300

    feat: agregar soporte para temas claro y oscuro en la aplicación

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,5 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 // import { useAuth } from './Context';
 import { EdicionProvider } from './EdicionContexto';
@@ -9,55 +9,88 @@ import Public from './layout/Public';
 import User from './layout/User';
 import Register from './layout/Registro';
 import theme from './theme/theme';
-import Vote from  './layout/Vote';
+import Vote from './layout/Vote';
+
+const BlackTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'black',
+        color: 'white',
+      },
+    },
+  },
+});
+
+const LightTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'white',
+        color: 'black',
+      },
+    },
+  },
+});
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState(theme);
+
   useEffect(() => {
     document.title = 'Bienal del Chaco';
   }, []);
 
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === theme ? BlackTheme : theme);
+  };
+
   // const { isAuthenticated } = useAuth();
 
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={currentTheme}>
       <EdicionProvider>
-      <BrowserRouter>
-        <Routes>
-           <Route path="/auth/*" element={<Auth />} />
-           <Route path="/voting/" element={<Vote />} />  
-           <Route path="/admin/*" element={<Admin />} />
-           <Route
+        <BrowserRouter>
+          <button
+          onClick={toggleTheme}
+          style={{
+            padding: '10px 10px',
+            backgroundColor: 'black',
+            color: 'white',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            fontSize: '16px',
+          }}
+          >
+          <img
+            src="/dark.jpg"
+            alt="Luna"
+            style={{
+            width: '20px',
+            height: '20px',
+            }}
+          />
+          </button>
+          <Routes>
+            <Route path="/auth/*" element={<Auth />} />
+            <Route path="/voting/" element={<Vote />} />
+            <Route path="/admin/*" element={<Admin />} />
+            <Route
               path="/admin/"
-              element={
-                <Navigate
-                  replace
-                  to='/admin/escultores' 
-                />
-              }
+              element={<Navigate replace to="/admin/escultores" />}
             />
-           <Route path="/public/*" element={<Public />} />
-           <Route
+            <Route path="/public/*" element={<Public />} />
+            <Route
               path="/public/"
-              element={
-                <Navigate
-                  replace
-                  to='/public/eventos' 
-                />
-              }
+              element={<Navigate replace to="/public/eventos" />}
             />
             <Route path="/user/*" element={<User />} />
-        
-           <Route
+            <Route
               path="/*"
-              element={
-                <Navigate
-                  replace
-                  to='/auth/' 
-                />
-              }
+              element={<Navigate replace to="/auth/" />}
             />
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
       </EdicionProvider>
     </ChakraProvider>
   );
