bd54e1909f0aa25ded29e8a9c064b04bac4b9b42
yoelmarain <jmarain62@gmail.com>
2024-11-16 01:45:41 -0300

    fix: recuperada interfaz de obra detalle

--- FrontEnd/src/pages/Public/Obras/ObraDetalle.tsx
+++ FrontEnd/src/pages/Public/Obras/ObraDetalle.tsx
@@ -26,6 +26,7 @@ const ObraDetail = () => {
   const userId = Cookies.get('IdUser');
   const { id } = useParams<{ id: string }>();
   const [obra, setObra] = useState<Obra | null>(null);
+  const [images, setImages] = useState<any[]>([]);
   const [isDisabled, setIsDisabled] = useState(false);
   const navigate = useNavigate();
   const { rolUser } = useAuth();
@@ -60,43 +61,127 @@ const ObraDetail = () => {
     }
   }, [obra, userId]);
 
+  useEffect(() => {
+    if (obra) {
+      const images = [
+        {
+          original: obra.imagenes,
+          thumbnail: obra.imagenes,
+        },
+      ];
+      setImages(images);
+    }
+  }, [obra]);
+
   const handleVotarClick = () => {
     navigate(`/user/voting/${id}/${userId}`);
   };
 
   return (
-    <Box display="flex" flexDirection="column" w="100%">
-      {obra && (
-        <Flex direction="column" w="100%">
-          <Box w="100%" minHeight="33vh"  textAlign={'center'} backgroundColor="#0B192C">
-            <Heading color="#CDC2A5" fontSize="5xl">{obra.nombre}</Heading>
-            <RedesSocialesLight />
+    <Box
+    display={'flex'}
+    flexDirection={{ base: 'column', md: 'row', lg: 'row' }}
+    w={'100%'}
+  >
+    {obra && (
+      <Flex direction={'column'}
+      justifyContent={'center'}
+      w={'100%'}>
+          <Box
+            w={'100%'}
+            minHeight={'33vh'} 
+            display={'flex'}
+            mb={2}
+            backgroundColor="#0B192C"
+            alignItems={'center'}
+            position={'relative'}
+            justifyContent={'space-around'}
+          >
+            <Heading ml={'7%'} color={'#CDC2A5'} fontSize={'5xl'}>
+              {obra.nombre}
+            </Heading>
+            <Flex>
+              <RedesSocialesLight />
+            </Flex>
           </Box>
-          <Flex justifyContent="space-between">
-            <ImageGallery
-              items={[{ original: obra.imagenes, thumbnail: obra.imagenes }]}
-              showPlayButton
-              autoPlay
-              slideInterval={5000}
-            />
-            <Box>
-              <Image src={obra.escultorImagen} boxSize="90px" borderRadius="full" />
-              <Heading size="sm">{obra.escultorNombre}</Heading>
-              <Text as="i">{obra.escultorPais}</Text>
-              <Text>Bajo la temática {obra.tematica}</Text>
-              <Text>Creada el {obra.fechaCreacion}</Text>
-              <Text>{obra.descripcion}</Text>
+
+          <Box
+            display="flex"
+            justifyContent={'space-between'}
+            w={{ base: '100%', md: '100%', lg: '100%' }}
+            flexDirection={'row'}
+          >
+            <Box p={4} 
+             top={"-35px"} 
+             position="relative"
+             zIndex={1}
+             >
+              <ImageGallery
+                items={images}
+                showPlayButton={true} // desactivo el boton de play
+                autoPlay={true} //activo para que arranquen solas
+                slideInterval={5000} //cada cuanto cambia, 4seg
+              />
+            </Box>
+            <Box
+              w={{ base: '100%', md: '100%', lg: '30%' }}
+              display="flex"
+              flexDirection={'column'}
+              mt={1}
+              mr={10}
+              className="Informacion"
+            >
+            <Flex gap="4" alignItems="center" 
+            justifyContent={'center'} mt={8}>
+              <Image
+                src={obra.escultorImagen}
+                boxSize="90px"
+                borderRadius="full"
+                borderWidth={1}
+                borderColor="azul"
+                borderStyle="solid"
+              />
+              <Box>
+                <Heading size="sm">{obra.escultorNombre}</Heading>
+                <Text as="i">{obra.escultorPais}</Text>
+              </Box>
+            </Flex>
+            <Box
+              mt={6}
+              display={'flex'}
+              textAlign={'right'}
+              flexDirection={'column'}
+              marginLeft={'auto'}
+              w={'100%'}
+            >
+              <Text as="em">Bajo la tematica {obra.tematica}</Text>
+              <Text as="em">Creada el {obra.fechaCreacion}</Text>
+              <Text textAlign={'left'} mt={6} ml={4}>
+                {obra.descripcion}
+              </Text>
               {isDisabled && <Text>Ya has votado por esta obra</Text>}
               { rolUser !== '' && !isDisabled &&
               <Button onClick={handleVotarClick} isDisabled={isDisabled}>Votar</Button>
               }
             </Box>
-          </Flex>
-          <Box mt={4} mb={4} w={'100%'} bg={'azul'}  p={10}>
-            <Text color={'beige'} fontSize={26}>Obras relacionadas</Text>
           </Box>
-          <ObrasRelacionadas esculturaId={obra.esculturaId} />
+          </Box>
+          <Flex direction={'column'}>
+            <Flex bg={'azul'} color={'beige'} w={'100%'} textAlign={'start'}
+            h={'15vh'}
+            mb={4}
+            alignItems={'center'}
+            >
+              <Text fontSize={24} ml={5} fontWeight={'bold'}>
+              Obras Relacionadas
+              </Text>
+            </Flex> 
+            <Flex>
+              <ObrasRelacionadas esculturaId={obra.esculturaId} />
+            </Flex>
+          </Flex>
         </Flex>
+        
       )}
     </Box>
   );
