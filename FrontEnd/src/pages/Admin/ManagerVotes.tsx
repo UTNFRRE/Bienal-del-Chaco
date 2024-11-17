0e9cc99b01b82c47f712ed6cfe198cd02b0a897e
Camilo Aguirre <camiloo12.c1@gmail.com>
2024-11-17 16:10:15 -0300

    podium

--- FrontEnd/src/pages/Admin/ManagerVotes.tsx
+++ FrontEnd/src/pages/Admin/ManagerVotes.tsx
@@ -102,7 +102,7 @@ const ManagerVotes = () => {
                         flexDirection={{base: "column", md: "row"}}
                         position="absolute"
                     >
-                        <Box height="100%" width={{md:"40%", base:"100%"}}>
+                        <Box height="100%" pr={4} width={{md:"40%", base:"100%"}}>
                              <DataTable data={Obras} /> 
                         </Box>
                         <Box  height="100%" width="60%">
